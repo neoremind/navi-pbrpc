6faf1f418ed1b197483456e80f38f957a5b47867
neoremind <notyetfish@hotmail.com>
2015-07-05 15:59:07 +0800

    modify cobertura test conf

--- src/test/java/com/baidu/beidou/navi/pbrpc/util/IdGeneratorTest.java
+++ src/test/java/com/baidu/beidou/navi/pbrpc/util/IdGeneratorTest.java
@@ -3,8 +3,11 @@ package com.baidu.beidou.navi.pbrpc.util;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
 public class IdGeneratorTest {
 
+    @Test
     public void testGetId() {
         int id = IdGenerator.genUUID();
         assertThat(id, greaterThan(0));
