900188d1f2aadaa31109b2b7c282289b60ebb34b
neoremind <notyetfish@hotmail.com>
2015-07-05 17:16:45 +0800

    change default timeout

--- src/main/java/com/baidu/beidou/navi/pbrpc/util/PbrpcConstants.java
+++ src/main/java/com/baidu/beidou/navi/pbrpc/util/PbrpcConstants.java
@@ -12,12 +12,12 @@ public class PbrpcConstants {
     /**
      * 默认客户端连接超时时间，单位毫秒
      */
-    public static final int DEFAULT_CLIENT_CONN_TIMEOUT = 2000;
+    public static final int DEFAULT_CLIENT_CONN_TIMEOUT = 5000;
 
     /**
      * 默认客户端调用读超时时间，单位毫秒
      */
-    public static final int DEFAULT_CLIENT_READ_TIMEOUT = 50000;
+    public static final int DEFAULT_CLIENT_READ_TIMEOUT = 60000;
 
     /**
      * 默认客户端超时调用检测器启动时间，单位毫秒
